2053f87e126165c5f87cbae618c12d51c9b1249e
Evgeny Startsev <startsev2000@outlook.com>
2021-05-10 22:04:01 +0300

    Started working with image adding

--- src/views/AddNews.vue
+++ src/views/AddNews.vue
@@ -5,10 +5,11 @@
         <label>Заголовок</label>
         <input v-model="form.heading" type="text" />
       </div>
-      <!-- <div class="photo">
+      <div class="photo">
         <label>Фотография</label>
         <input type="file" />
-      </div>-->
+        <button @click="onUpload">Upload image</button>
+      </div>
       <div class="news1">
         <label>Новость</label>
         <textarea rows="10" cols="40" v-model="form.news"></textarea>
@@ -21,8 +22,9 @@
 </template>
 
 <script>
-import { addNews } from '@/main'
-import { reactive } from 'vue'
+import { addNews } from "@/main";
+import { reactive } from "vue";
+import firebase from 'firebase'
 
 export default {
   /*
@@ -32,18 +34,41 @@ export default {
     Профессия
     Описание
     */
-   setup() {
-       const form = reactive({ heading: '',  news: '' })
+  setup() {
+    const form = reactive({ heading: "", news: "" });
 
-       const submitNews = async () => {
-           await addNews({...form})
-           form.heading = ''
-           form.news = ''
-       }
+    const submitNews = async () => {
+      await addNews({ ...form });
+      form.heading = "";
+      form.news = "";
+    };
 
-       return { form, submitNews }
-   }
+    const onUpload = () => {
+      this.picture = null;
+      const storageRef = firebase
+        .storage()
+        .ref(`${this.imageData.name}`)
+        .put(this.imageData);
+      storageRef.on(
+        `state_changed`,
+        (snapshot) => {
+          this.uploadValue =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+          console.log(error.message);
+        },
+        () => {
+          this.uploadValue = 100;
+          storageRef.snapshot.ref.getDownloadURL().then((url) => {
+            this.picture = url;
+          });
+        }
+      );
+    }
 
+    return { form, submitNews, onUpload };
+  },
 };
 </script>
 
